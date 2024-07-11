d48da92c81380890431d9f771c68eaee60f11ff3
mahinahm3d <mahinahmed.prof@gmail.com>
2024-07-11 20:51:33 +0500

    Add files via upload

---
+++ 5. FAMOUS QUOTE 2/app.ts
@@ -0,0 +1,4 @@
+// Famous Quote 2
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, "A person who never made a mistake never tried anything new."`;
+console.log(message);
